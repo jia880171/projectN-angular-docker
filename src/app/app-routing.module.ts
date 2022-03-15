102bee9004884ed23347939218800e1472f3a08c
j_lin <j_lin@zisouz.co.jp>
2022-03-15 13:42:23 +0900

    clean start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
-import { YoloDemoComponent } from './component/yolo-demo/yolo-demo.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: 'yoloDemo',
-    component: YoloDemoComponent,
-  },
-  {
-    path: '**',
-    component: AppComponent,
-  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { YoloDemoComponent } from './component/yolo-demo/yolo-demo.component';
 
 @NgModule({
-  declarations: [AppComponent, YoloDemoComponent],
+  declarations: [AppComponent ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
