75f53c988f41848c76bd231631d79b1fb0856b73
j_lin <j_lin@zisouz.co.jp>
2022-03-15 11:00:02 +0900

    feat: yolo webcam demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { YoloDemoComponent } from './component/yolo-demo/yolo-demo.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: 'yoloDemo',
+    component: YoloDemoComponent,
+  },
   {
     path: '**',
     component: AppComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YoloDemoComponent } from './component/yolo-demo/yolo-demo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, YoloDemoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
